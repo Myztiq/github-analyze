ea0a0f70ea82da07c1e71a34ca5fd50d035d73fc
Ryan Kahn <ryan.kahn@amplitude.com>
2021-02-22 09:34:48 -0800

    Fetch team mapping from config

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const extractArray = (str) => {
+    return str.split(',').map((item) => {
+        return item.trim()
+    })
+}
+
 const run = async () => {
     // Get octokit
     const gitHubToken = core.getInput('repo-token', { required: true });
-    const teams = core.getInput('teams', { required: true });
+    const teams = extractArray(core.getInput('teams', { required: true }));
     const octokit = github.getOctokit(gitHubToken);
 
-    console.log(gitHubToken, teams, octokit)
+    const teamMapping = teams.reduce((accum, team) => {
+        const members = extractArray(core.getInput(`team-${team}`, { required: true }));
+        accum[team] = members
+
+        return accum
+    }, {})
+
+    console.log(teamMapping);
 }
 
 try {
